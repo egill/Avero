4bfd14af58887aecba0ad32685229fcf7ab2384a
Egill E <egill@rsf.is>
2026-01-10 22:34:13 +0100

    chore: remove command build artifacts

--- command/_build/test/lib/avero_command/priv/repo/migrations/20260106231000_events_person_id_to_bigint.exs
+++
@@ -1,9 +0,0 @@
-defmodule AveroCommand.Repo.Migrations.EventsPersonIdToBigint do
-  use Ecto.Migration
-
-  def change do
-    alter table(:events) do
-      modify :person_id, :bigint, from: :integer
-    end
-  end
-end
